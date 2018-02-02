a54a77442bbb6d9846bb52fc8622d9abcbec7be8
BitBoy92 <spector.8500@gmail.com>
2018-02-01 22:22:04 -0500

    WARNING, copy and pasting doesn't copy the file to the project. Oh how weird you are VS!

---
+++ MyEFPSolutions/TipCalculator/TipCalculator.cpp
@@ -0,0 +1,45 @@
+// Author: Ben Stone
+// Date of Last Update: 29 Jan 2018
+// Exercise Description: Create a console based program to calculate the tip for a transaction
+// Page in Exercises for Programmers: 6
+
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+	// VARIABLE INIT
+	float billAmount = 0.0f;
+	float tip = 0.0f;
+	int tipRate = 0; // tip percentage expressed as an integer.
+	float total = 0.0f;
+	// END VI
+
+	// PROMPT FOR USER INPUT
+	cout << "What is the bill amount?" << endl;
+	cin >> billAmount;
+
+	cout << "What is the tip rate?" << endl;
+	cin >> tipRate;
+	// END PFUI
+
+	// CALC TIP
+	tip = billAmount * (tipRate * 0.01f);
+	tip = ceilf(tip * 100) / 100; // want to round up to the .0X place.
+								  // END CT
+
+								  // CALC TOTAL
+	total = billAmount + tip;
+	// END CT
+
+	// DISPLAY OUTPUT
+	cout.setf(ios::fixed); // fixed floating point
+	cout.precision(2); // 2 digits after decimal
+	cout << "Tip: $" << tip << endl;
+	cout << "Total: $" << total << endl;
+	// END DO
+
+	cin.get(); //Prevent terminal from closing until user input
+}
